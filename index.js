b3b63e4f17dca22daafd3a95f517a895977cc4f8
Vanessa Jesik <vajesik@gmail.com>
2023-08-06 23:39:45 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name: "Steve", email: "steve@steve.com" }),
+  })
+    .then(response => response.json())
+    .then(data => document.querySelector("body").append(data.id))
+    .catch(error => document.querySelector("body").append(error.message));
+}
